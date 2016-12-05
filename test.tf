dddcd62c1dc065b9e1803bd9537fc70ef3a75847
Simon Jefford <simon.jefford@fourth.com>
2016-12-05 16:18:14 +0000

    test some more things

--- test.tf
+++ test.tf
@@ -2,12 +2,17 @@ provider "sjj" {
   user = "simon"
 }
 
-resource "sjj_test" "someresource" {
+resource "sjj_test" "foobar" {
   name = "foobar.txt"
-  content = "hello world 2"
+  content = "hello world!!!!!!!!!!\n"
 }
 
-resource "sjj_test" "newresource" {
-  name = "bar.txt"
-  content = "${sjj_test.someresource.name}"
+resource "sjj_test" "baz" {
+  count = 10
+  name = "baz${count.index}.txt"
+  content = "This is file ${count.index}. ${sjj_test.foobar.name} is ${sjj_test.foobar.size} bytes big\n"
+}
+
+output "foobar-size" {
+  value = "${sjj_test.foobar.size}"
 }
